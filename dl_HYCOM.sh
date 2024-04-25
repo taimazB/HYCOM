4260c94f68d8de89aa8ddb18d5729efb1ce0b8b8
TaimazB <taimaz@gmail.com>
2024-04-25 16:36:53 -0600

    fixed.

--- dl_HYCOM.sh
+++ dl_HYCOM.sh
@@ -44,7 +44,7 @@ export -f DnP
 ############################################################################
 
 rm -r ${MAIN}/nc ${MAIN}/extracted ${MAIN}/tiles 2>/dev/null
-mkdir -p ${MAIN}/nc ${MAIN}/extracted ${MAIN}/tiles/temperature
+mkdir -p ${MAIN}/nc ${MAIN}/extracted ${MAIN}/tiles/temperature ${MAIN}/tiles/salinity ${MAIN}/tiles/density
 mkdir ${MAIN}/logs 2>/dev/null
 cd ${MAIN}/nc
 parallel -j 8 'DnP {}' ::: $(seq 0 3 180)
