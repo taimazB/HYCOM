644885bf454ea73034d36b27248d24e4a0d73846
TaimazB <taimaz@gmail.com>
2024-04-26 07:57:20 -0600

    cleanup modified.

--- cleanup.sh
+++ cleanup.sh
@@ -5,7 +5,7 @@ if [[ -e .active ]] && [[ ! -e .cleanup ]] && [[ $n -eq 0 ]]; then
 
     rsync -aur -e "ssh -p ${SERVER_PORT}" tiles ${SERVER_IP}:${SERVER_DIR}
     rsync -aur -e "ssh -p ${SERVER_PORT}" extracted/ ${SERVER_IP}:${SERVER_DIR}
-    rm -r extracted/*
+    rm -r extracted/
     rm -r nc/
     rm .active .cleanup
 fi
