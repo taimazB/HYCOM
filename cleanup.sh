901b0ba277dce2d9a722c178bf958004c29806aa
TaimazB <taimaz@gmail.com>
2024-04-29 09:30:56 -0600

    cleanup updated.

--- cleanup.sh
+++ cleanup.sh
@@ -3,8 +3,8 @@ if [[ -e .active ]] && [[ ! -e .cleanup ]] && [[ $n -eq 0 ]]; then
     touch .cleanup
     source ./configs.sh
 
-    rsync -aur -e "ssh -p ${SERVER_PORT}" tiles ${SERVER_IP}:${SERVER_DIR}
-    rsync -aur -e "ssh -p ${SERVER_PORT}" extracted/ ${SERVER_IP}:${SERVER_DIR}
+    rsync -aur --remove-source-files -e "ssh -p ${SERVER_PORT}" tiles ${SERVER_IP}:${SERVER_DIR}
+    rsync -aur --remove-source-files -e "ssh -p ${SERVER_PORT}" extracted/ ${SERVER_IP}:${SERVER_DIR}
     rm -r extracted/
     rm -r nc/
     rm .active .cleanup
