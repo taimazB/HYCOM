bdfe0e893dd6b6f9201eadceb9ddf2ed7f7cedd2
taimaz <tb4764@mun.ca>
2024-05-29 13:50:27 -0600

    debug

--- process.sh
+++ process.sh
@@ -12,14 +12,14 @@ source ../configs.sh
 date
 
 
-python3 ${MAIN}/scripts/cnvMaster_RGBcoded.py --fileName=$f --minZoom=2 --maxZoom=4
+# python3 ${MAIN}/scripts/cnvMaster_RGBcoded.py --fileName=$f --minZoom=2 --maxZoom=4
 
 date=$(echo $f | cut -d_ -f4 | sed 's/12$//')
 hr=$(echo $f | cut -d_ -f5 | sed 's/t0*//')
 saveDateTime=$(date -d "${date} 12 +${hr} hours" +%Y%m%d_%H%M)
-s3cmd put --recursive --acl-public tiles/temperature/${saveDateTime} s3://modeltiles/HYCOM/${date}/temperature/ &
-s3cmd put --recursive --acl-public tiles/salinity/${saveDateTime} s3://modeltiles/HYCOM/${date}/salinity/ &
-s3cmd put --recursive --acl-public tiles/density/${saveDateTime} s3://modeltiles/HYCOM/${date}/density/ &
+s3cmd put --recursive --acl-public ${MAIN}/tiles/temperature/${saveDateTime} s3://modeltiles/HYCOM/${date}/temperature/ &
+s3cmd put --recursive --acl-public ${MAIN}/tiles/salinity/${saveDateTime} s3://modeltiles/HYCOM/${date}/salinity/ &
+s3cmd put --recursive --acl-public ${MAIN}/tiles/density/${saveDateTime} s3://modeltiles/HYCOM/${date}/density/ &
 
 
 echo -e "`date +%F_%T`\t$f" >> ${MAIN}/.processed
