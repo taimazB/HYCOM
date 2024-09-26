76ec828ee5a514995cdae470a2b177ff19a353aa
taimaz <tb4764@mun.ca>
2024-09-26 16:48:31 -0600

    fix

--- finalize.sh
+++ finalize.sh
@@ -8,7 +8,7 @@ if [[ $n -eq 0 ]]; then
     ## UPDATE SERVER
     lastProcessedDate=$(tail -1 .processed | cut -f2 | cut -d_ -f4 | sed 's/12$//')
     n=`grep _${lastProcessedDate} ${MAIN}/.processed | wc -l`
-    if [[ $n -eq 360 ]]; then
+    if [[ $n -eq 300 ]]; then
         ssh root@${SERVER_IP} <<EOF
 mv ${SERVER_DIR} ${SERVER_DIR}.old && mv ${SERVER_DIR}.new ${SERVER_DIR} && rm -r ${SERVER_DIR}.old
 cd ${SERVER_DIR}
