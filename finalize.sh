004e4373df1c7cb8a95a8d3b667a962853bf926c
taimaz <tb4764@mun.ca>
2024-10-16 18:59:53 -0230

    Download lastAvailDate

--- finalize.sh
+++ finalize.sh
@@ -10,11 +10,9 @@ if [[ $n -eq 0 ]]; then
     n=`grep _${lastProcessedDate} ${MAIN}/.processed | wc -l`
     if [[ $n -eq 300 ]]; then
         ssh root@${SERVER_IP} <<EOF
-if [[ -e ${SERVER_DIR} ]]; then
 mv ${SERVER_DIR} ${SERVER_DIR}.old
-fi
 mv ${SERVER_DIR}.new ${SERVER_DIR}
-# rm -r ${SERVER_DIR}.old
+rm -r ${SERVER_DIR}.old
 cd ${SERVER_DIR}
 python3 ../updateAvails.py
 EOF
